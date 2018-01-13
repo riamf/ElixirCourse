6d9dfc8cbf11ecb5b6fef6600004c0136f1c1aa1
riamf <pawelkowalczuk@230415071.local>
2018-01-13 11:35:02 +0100

    I/O operations, documentation and testing

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,15 +1,71 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
 
+  @doc """
+    Returns a list of string representing deck of cards
+  """
   def create_deck do
-    ["Ace", "Two", "Three"] # Double quotes is common elixir convention
+    values = ["Ace", "Two", "Three", "Four", "Five"] # Double quotes is common elixir convention
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+    
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
   end
 
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determins whether deck contains card
+
+  ## Examples
+
+        iex> deck = Cards.create_deck
+        iex> Cards.contains?(deck, "Ace of Spades")
+        true
+        iex> Cards.contains?(deck, "Unknown")
+        false
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Devides a deck into a hand and remainder of the deck.
+    The `hand_size` arguments indicates how many cards should
+    be in hand.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, _deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+  """
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "File does not exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
+  end
 end
